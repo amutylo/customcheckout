812ab351810e713573acff6f2a4963dd2960d68e
Andrii Mutylo <amutylo@gmail.com>
2015-11-25 21:13:59 +0200

    add new code

--- app/code/community/A2m/Customcheckout/Helper/Data.php
+++ app/code/community/A2m/Customcheckout/Helper/Data.php
@@ -8,10 +8,16 @@
 class A2m_Customcheckout_Helper_Data extends Mage_Core_Helper_Abstract
 {
     const XML_CHECKOUT_OPTIONS_CUSTOM_CHECKOUT_ENABLED = 'checkout/options/custom_checkout_enabled';
+    const CHECKOUT_TYPE_ONEPAGE = 'onepage';
+    const CHECKOUT_TYPE_ONESTEP = 'onestep';
+
 
     public function customCheckoutEnabled()
     {
         return (bool)Mage::getStoreConfig(self::XML_CHECKOUT_OPTIONS_CUSTOM_CHECKOUT_ENABLED);
     }
 
+    public function getCheckoutType(){
+        return (bool)Mage::getStoreConfig(self::XML_CHECKOUT_OPTIONS_CUSTOM_CHECKOUT_TYPE);
+    }
 }
--- app/code/community/A2m/Customcheckout/Model/Type/Customcheckout.php
+++ app/code/community/A2m/Customcheckout/Model/Type/Customcheckout.php
@@ -18,4 +18,76 @@ class A2m_Customcheckout_Model_Customcheckout extends Mage_Checkout_Model_Type_O
 
     }
 
+    /**
+     * Initialize quote state to be valid for one page checkout
+     *
+     * @return Mage_Checkout_Model_Type_Onepage
+     */
+    public function initCheckout()
+    {
+        $checkoutHelper = $this->_getHelper();
+        if($checkoutHelper::CHECKOUT_TYPE_ONEPAGE == $checkoutHelper->getCheckoutType()){
+            $checkout = $this->getCheckout();
+            $customerSession = $this->getCustomerSession();
+            if (is_array($checkout->getStepData())) {
+                foreach ($checkout->getStepData() as $step=>$data) {
+                    if (!($step==='login' || $customerSession->isLoggedIn() && $step==='billing')) {
+                        $checkout->setStepData($step, 'allow', false);
+                    }
+                }
+            }
+        }
+
+
+        $quoteSave = false;
+        $collectTotals = false;
+
+        /**
+         * Reset multishipping flag before any manipulations with quote address
+         * addAddress method for quote object related on this flag
+         */
+        if ($this->getQuote()->getIsMultiShipping()) {
+            $this->getQuote()->setIsMultiShipping(false);
+            $quoteSave = true;
+        }
+
+        /**
+         *  Reset customer balance
+         */
+        if ($this->getQuote()->getUseCustomerBalance()) {
+            $this->getQuote()->setUseCustomerBalance(false);
+            $quoteSave = true;
+            $collectTotals = true;
+        }
+        /**
+         *  Reset reward points
+         */
+        if ($this->getQuote()->getUseRewardPoints()) {
+            $this->getQuote()->setUseRewardPoints(false);
+            $quoteSave = true;
+            $collectTotals = true;
+        }
+
+        if ($collectTotals) {
+            $this->getQuote()->collectTotals();
+        }
+
+        if ($quoteSave) {
+            $this->getQuote()->save();
+        }
+
+        /*
+        * want to load the correct customer information by assigning to address
+        * instead of just loading from sales/quote_address
+        */
+        $customer = $customerSession->getCustomer();
+        if ($customer) {
+            $this->getQuote()->assignCustomer($customer);
+        }
+        return $this;
+    }
+
+    protected function _getHelper(){
+        return Mage::helper('a2m_customcheckout');
+    }
 }
